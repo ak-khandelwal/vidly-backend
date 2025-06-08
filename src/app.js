75189bf17a621d49b8768d548ada3af431bb3d1f
aryan khandelwal <aryankhandelwal15@gmail.com>
2025-06-08 19:20:04 +0530

    added prod vs dev handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cors({
-    origin:"http://192.168.1.173:5173",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://192.168.1.173:5173",
+    credentials: true,
+  })
+);
 
-
-app.use(express.json({limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }));
 // app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-app.use(cookieParser())
-const userRouter = require('./routes/user.routes.js');
+app.use(express.static("public"));
+app.use(cookieParser());
+const userRouter = require("./routes/user.routes.js");
 const healthCheckRouter = require("./routes/healthCheck.routes.js");
 const tweetRouter = require("./routes/tweet.routes.js");
 const subscriptionRouter = require("./routes/subscription.routes.js");
@@ -24,13 +25,13 @@ const likeRouter = require("./routes/like.routes.js");
 const playlistRouter = require("./routes/playlist.routes.js");
 const dashboardRouter = require("./routes/dashboard.routes.js");
 
-app.use("/api/v1/healthCheck", healthCheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-module.exports = app 
+app.use("/api/v1/healthCheck", healthCheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-require('dotenv').config();
-const connectDB = require('./db/index');
-const app = require('./app');
+require("dotenv").config();
+const connectDB = require("./db/index");
+const app = require("./app");
 const port = process.env.PORT || 5000;
-const host = '0.0.0.0' || 'localhost';
-;(async ()=>{
-    try{
-        await connectDB();
-        app.listen(port,host,function(){
-            console.log(`server is running on port http://${host}:${port}`)
-        })
+const host = "0.0.0.0";
+(async () => {
+  try {
+    await connectDB();
+    if (process.env.DEVELOPMENT === "true") {
+      app.listen(port, host, function () {
+        console.log(`server is running on port http://${host}:${port}`);
+      });
+    } else {
+      app.listen(port, function () {
+        console.log(`Server is running on port ${port}`);
+      });
     }
-    catch(error){
-        console.log("MONGODB connect failed! ",error);
-    }
-    
-})()
+  } catch (error) {
+    console.log("MONGODB connect failed! ", error);
+  }
+})();
