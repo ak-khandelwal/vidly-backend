73d8387828c8bfd6e6672dce37ded38b29ee4b3d
aryan khandelwal <aryankhandelwal15@gmail.com>
2025-06-09 14:54:15 +0530

    cloudinary will save assets on folder named vidly

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,36 +1,36 @@
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_SECRET_KEY // Click 'View API Keys' above to copy your API secret
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY, // Click 'View API Keys' above to copy your API secret
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+      folder: "vidly",
+    });
+    fs.unlinkSync(localFilePath);
+    console.log("file is uploaded on cloudinary ", res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+const deleteImageFromCloudinary = async (publicId) => {
+  try {
+    if (!publicId) return null;
+    const res = await cloudinary.uploader.destroy(publicId);
+    console.log("file is deleted from cloudinary ", res);
+    return res;
+  } catch (error) {
+    return null;
+  }
+};
 
-const uploadOnCloudinary = async (localFilePath) =>{
-    try{
-        if(!localFilePath) return null;
-        const res = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:'auto',
-        })
-        fs.unlinkSync(localFilePath);
-        console.log("file is uploaded on cloudinary ",res.url);
-        return res
-    }catch(error){
-        fs.unlinkSync(localFilePath);
-        return null;
-    }
-}
-const deleteImageFromCloudinary = async (publicId) =>{
-    try{
-        if(!publicId) return null;
-        const res = await cloudinary.uploader.destroy(publicId)
-        console.log("file is deleted from cloudinary ",res);
-        return res
-    }catch(error){
-        return null;
-    }
-}
-
-module.exports = {uploadOnCloudinary,deleteImageFromCloudinary}
+module.exports = { uploadOnCloudinary, deleteImageFromCloudinary };
